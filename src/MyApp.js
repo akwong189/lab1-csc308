824ecb840f93c75dd495c6c38a05d2ecda61e5d2
Alterac8 <awong189@calpoly.edu>
2021-01-08 18:49:03 -0800

    added a form component

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,13 +1,9 @@
 import React, {useState} from 'react';
 import Table from './Table'
+import Form from './Form'
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: 'Charlie',
-            job: 'Janitor'
-        },
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter( (character, i) => {
@@ -15,10 +11,11 @@ function MyApp() {
         });
         setCharacters(updated)
     }
-
+    
     return (
       <div className="container">
           <Table characterData={characters} removeCharacter={removeOneCharacter}/>
+          <Form />
       </div>
     );
 }
